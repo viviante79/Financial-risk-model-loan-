9be0658e03aec32ca0b6a230f60e6c63c76b6e81
viviante79 <104625942+viviante79@users.noreply.github.com>
2022-09-09 17:34:39 +0200

    Add files via upload

---
+++ DS7 - API_Streamlit.py
@@ -0,0 +1,23 @@
+# Import des librairies :
+import pandas as pd
+import streamlit as st
+import joblib
+
+# Import des fichiers, du modèle et du scaler :
+data = pd.read_csv("dataset/data_api.csv", index_col=[0])
+model = joblib.load("dataset/LGBM_model")
+scaler = joblib.load("dataset/Scaler")
+
+# Filtre :
+id_filter = st.selectbox("Entrez identifiant client", pd.unique(data["SK_ID_CURR"]))
+df = data[data["SK_ID_CURR"] == id_filter]
+
+# Affichage de la prédiction :
+query = df.drop(columns=["TARGET","SK_ID_CURR"])
+score = list(model.predict_proba(query))
+st.json({"Id" : int(df["SK_ID_CURR"]), "score" : score[0][0]})
+
+query_all = data.drop(columns=["TARGET","SK_ID_CURR"])
+score_all = list(model.predict_proba(query_all))
+dictionnary = dict(zip(data["SK_ID_CURR"], score_all))
+st.json(dictionnary)
